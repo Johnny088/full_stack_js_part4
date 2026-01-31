22237e0d738ce5dcdb469f5203ef0fd142ec399c
Johnny <esp25goodlife@gmail.com>
2026-01-31 07:33:18 +0200

    added local data variables instead global

--- main.js
+++ main.js
@@ -40,82 +40,81 @@ let books = [
 ];
 
 const root = document.querySelector('#root');
-function startHtml() {
-  const list = document.createElement('ul');
-  list.classList.add('list');
-  const container = document.createElement('div');
-  const descriptionDiv = document.createElement('div');
-  descriptionDiv.classList.add('info');
-  container.className = 'container';
-  root.innerHTML = '<h1 class="title">List of books</h1>';
-  root.append(container);
+const list = document.createElement('ul');
+list.classList.add('list');
+const container = document.createElement('div');
+const descriptionDiv = document.createElement('div');
+descriptionDiv.classList.add('info');
+container.className = 'container';
+root.innerHTML = '<h1 class="title">List of books</h1>';
+root.append(container);
 
-  //   ------------------------------ adding the button to add a new book -----------------------
-  const addBtn = document.createElement('button');
-  addBtn.textContent = 'add new book';
-  addBtn.classList.add('form_btn');
-  gettingBooks(list);
-  container.append(list, descriptionDiv, addBtn);
-  bookHandler(descriptionDiv, list);
-  addBookHandler(descriptionDiv, list);
-}
-// ---------------------------- getting books ---------------------
-const gettingBooks = list => {
+//   ------------------------------ adding the button to add a new book -----------------------
+const addBtn = document.createElement('button');
+addBtn.textContent = 'add new book';
+addBtn.classList.add('form_btn');
+gettingBooks();
+container.append(list, descriptionDiv, addBtn);
+bookHandler();
+addBookHandler();
+// ---------------------------- getting books first time ---------------------
+function gettingBooks() {
   let data = getData();
   if (!data || data.length === 0) {
-    setData();
+    setData(books);
     data = getData();
   }
-  books = data;
-  renderBooks(list);
-};
-
-const renderBooks = list => {
-  const booksList = books
+  renderBooks(data);
+}
+// -----------------------------------render list of books -----------------------------
+function renderBooks(data) {
+  const booksList = data
     .map(({ title, id }) => {
       return `<li id="${id}"><h3>${title}</h3><button>view details</button><button class="delete">delete</button></li>`;
     })
     .join('');
   list.innerHTML = booksList;
-};
+}
 // ----------------------------------------- button handler to describe & to delete books  ------------------------
-const bookHandler = (descriptionDiv, list) => {
+function bookHandler() {
   list.addEventListener('click', e => {
-    if (
-      e.target.nodeName === 'BUTTON' &&
-      e.target.textContent === 'view details'
-    ) {
-      const id = Number(e.target.parentNode.id);
-      const { title, author, year, description } = books.find(
-        book => book.id === id,
-      );
-      descriptionDiv.innerHTML = `<h3>${title}</h3> <p>${author}</p> <p>${year}</p> <p>${description}</p>`;
-    } else if (
-      e.target.nodeName === 'BUTTON' &&
-      e.target.textContent === 'delete'
-    ) {
-      console.log(e.target.textContent);
-      const id = Number(e.target.parentNode.id);
-      e.target.parentNode.remove();
-      const removed = setTimeout(
-        () => (descriptionDiv.innerHTML = `<h2>the book was deleted</h2>`),
-        1000,
-      );
-      setTimeout(() => (descriptionDiv.innerHTML = ''), 4000);
-      books = books.filter(item => item.id !== id);
-      localStorage.setItem('booksData', JSON.stringify(books));
+    if (e.target.nodeName === 'BUTTON') {
+      let data = getData();
+      if (data.length === 0) return;
+      if (e.target.textContent === 'view details') {
+        {
+          const id = Number(e.target.parentNode.id);
+          const book = data.find(book => book.id === id);
+          if (!book) return;
+          const { title, author, year, description } = book;
+          descriptionDiv.innerHTML = `<h3>${title}</h3> <p>${author}</p> <p>${year}</p> <p>${description}</p>`;
+        }
+      } else if (e.target.textContent === 'delete') {
+        // console.log(e.target.textContent);
+        const id = Number(e.target.parentNode.id);
+        // e.target.parentNode.remove();
+        data = data.filter(item => item.id !== id);
+        setData(data);
+        renderBooks(data);
+        setTimeout(() => {
+          descriptionDiv.innerHTML = `<h2>the book was deleted</h2>`;
+        }, 1000);
+        setTimeout(() => {
+          descriptionDiv.innerHTML = '';
+        }, 4000);
+      }
     }
   });
-};
-
+}
 // ----------------------------------lisntener for adding books ----------------------
-const addBookHandler = (descriptionDiv, list) => {
+function addBookHandler() {
   const addNewBook = document.querySelector('.form_btn');
-  addNewBook.addEventListener('click', () => addBook(descriptionDiv, list));
-};
+  addNewBook.addEventListener('click', () => addBook());
+}
 
 // ----------------------------------------
-const addBook = (descriptionDiv, list) => {
+function addBook() {
+  let data = getData();
   descriptionDiv.innerHTML = '';
   if (root.querySelector('form')) {
     return;
@@ -130,13 +129,13 @@ const addBook = (descriptionDiv, list) => {
   descriptionDiv.append(form);
   form.addEventListener('submit', e => {
     e.preventDefault();
-    const title = form.bookTitle.value;
-    const author = form.author.value;
+    const title = form.bookTitle.value.trim();
+    const author = form.author.value.trim();
     const year = Number(form.year.value);
-    const description = form.description.value;
+    const description = form.description.value.trim();
     let error = '';
-    if (Number.isNaN(year) || year <= 0) {
-      error += '1) - Year must be a number, and >= 0\n';
+    if (Number.isNaN(year) || year <= 0 || !Number.isInteger(year)) {
+      error += '1) - Year must be an integer, and >= 0\n';
     }
     if (!title) {
       error += '2) - The title is requared\n';
@@ -151,7 +150,8 @@ const addBook = (descriptionDiv, list) => {
       alert(error);
       return;
     }
-    const id = books.reduce((acc, books) => Math.max(acc, books.id), 0) + 1;
+    const id =
+      data.reduce((acc, booksData) => Math.max(acc, booksData.id), 0) + 1;
     const newBook = {
       id: id,
       title: title,
@@ -159,32 +159,19 @@ const addBook = (descriptionDiv, list) => {
       year: year,
       description: description,
     };
-    books.push(newBook);
-    AddOneBook(newBook);
-    renderNewBook(newBook, list);
+    data = [...data, newBook];
+    console.log(data);
+    setData(data);
+    renderBooks(data);
     form.remove();
   });
-};
-// -------------------------- rendering a new book -------------------
-function renderNewBook(book, list) {
-  const item = document.createElement('li');
-  item.id = book.id;
-  item.innerHTML = `<h3>${book.title}</h3><button>view details</button><button class="delete">delete</button></li>`;
-  list.append(item);
 }
 
 // -------------------------local storage -----------------------------------
-function AddOneBook(book) {
-  let data = getData();
-  data = [...data, book];
+function setData(data) {
   localStorage.setItem('booksData', JSON.stringify(data));
 }
-const setData = () => {
-  localStorage.setItem('booksData', JSON.stringify(books));
-};
-const getData = () => {
-  const data = JSON.parse(localStorage.getItem('booksData'));
+function getData() {
+  const data = JSON.parse(localStorage.getItem('booksData')) || [];
   return data;
-};
-startHtml();
-// ------------------
+}
